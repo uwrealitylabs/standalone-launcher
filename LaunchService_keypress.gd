293025eb092c138b7601a4f0ef9f5a7db565515b
Oliver Lazar <oliver.lazar01@gmail.com>
2026-01-23 15:57:51 -0500

    Added application launching
    With Keyboard Input + UI Button

---
+++ LaunchService_keypress.gd
@@ -0,0 +1,22 @@
+extends Node
+
+func open_app():
+	match OS.get_name():
+		"Windows":
+			var pid = OS.create_process("powershell", [], true)
+			if pid != 1:
+				print("Process created with PID: ", pid)
+			else:
+				print("Failed to create process.")
+		"Linux", "X11":
+			var path = "weston-terminal" # in quotes place the path or application name you want launched
+			var args = [] # whatever arguments you want the app to launch with
+			var pid = OS.create_process(path, args, true) # final boolean is wheater you want a to open the console or not
+			if pid != 1:
+				print("Process created with PID: ", pid)
+			else:
+				print("Failed to create process.")
+				
+func _unhandled_input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_O:
+		open_app()
